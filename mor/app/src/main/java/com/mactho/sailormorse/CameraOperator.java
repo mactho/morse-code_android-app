9c9690adfb1bcf7320be68226139116ba9db4dcf
mactho <thomas.macneil.public@gmail.com>
2017-05-13 21:43:46 +1000

    Multithreaded and Permissions added, flash works

--- mor/app/src/main/java/com/mactho/sailormorse/CameraOperator.java
+++ mor/app/src/main/java/com/mactho/sailormorse/CameraOperator.java
@@ -1,7 +1,27 @@
 package com.mactho.sailormorse;
 
+import android.hardware.Camera;
+
+
 /**
  * Created by thomas on 13/05/17.
  */
-public class CameraOperator {
+public class CameraOperator implements Runnable{
+
+    @Override
+    public void run(){
+
+        Camera camera = Camera.open();
+        Camera.Parameters params = camera.getParameters();
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(params);
+        camera.startPreview();
+        try{
+            Thread.sleep(3000);
+        } catch( Exception e ){
+
+        }
+        camera.stopPreview();
+        camera.release();
+    }
 }
--- mor/app/src/main/java/com/mactho/sailormorse/MainActivity.java
+++ mor/app/src/main/java/com/mactho/sailormorse/MainActivity.java
@@ -1,39 +1,90 @@
 package com.mactho.sailormorse;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int CAMERA_PERMISSION_CODE = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                // TODO: Make an explaination dialog box
+                System.out.println("explain");
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, CAMERA_PERMISSION_CODE);
+            }
+        }
+
+        setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
+    public void sendMessage(View view) {
         StringToMorseTransformer transformer = new StringToMorseTransformer();
 
         // Get message from the message EditText
-        EditText messageTextView = (EditText)findViewById(R.id.message);
+        EditText messageTextView = (EditText) findViewById(R.id.message);
         String message = messageTextView.getText().toString();
 
         // Transform the human readable message into morse code
         String morseCode = transformer.transform(message);
 
         // Display the morse code in the morse code TextView
-        TextView morseTextView = (TextView)findViewById(R.id.morse_viewer);
+        TextView morseTextView = (TextView) findViewById(R.id.morse_viewer);
         morseTextView.setText(morseCode);
 
+        requestPermissions();
     }
 
-    public void clearMessage(View view){
-        EditText messageTextView = (EditText)findViewById(R.id.message);
+    public void requestPermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_CONTACTS)) {
+                // TODO: Make an explaination dialog box
+                System.out.println("explain");
+
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+            }
+        }
+    }
+
+    public void clearMessage(View view) {
+        EditText messageTextView = (EditText) findViewById(R.id.message);
         messageTextView.setText("");
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case CAMERA_PERMISSION_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CameraOperator cameraOperator = new CameraOperator();
+                    Executor executor = Executors.newSingleThreadExecutor();
+                    executor.execute(cameraOperator);
+                } else {
+                    // Permission denied path....
+                }
+                return;
+
+            // Other permissions (cases)...
+        }
     }
 }
